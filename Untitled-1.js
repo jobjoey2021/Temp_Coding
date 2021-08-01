9cecba53b00c2108dc3cc820a542b8424de8cb53
joeywda <55481103+joeywda@users.noreply.github.com>
2021-08-01 19:58:58 +0800

    0801 差 object to array
    差 object to array
    https://ithelp.ithome.com.tw/questions/10113382

---
+++ Untitled-1.js
@@ -0,0 +1,6 @@
+var jsObj = { name: "tom", age: 15 }; 
+var jsObjstr = JSON.stringify(jsObj); 
+console.log(jsObjstr); 
+var jsonObj = { "name": "jerry", "age": 16 }; 
+var jsonObjStr = JSON.stringify(jsonObj); 
+console.log(jsonObjStr);
